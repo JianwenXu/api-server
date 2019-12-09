b044a51282f676812ee585ec87006f19d8464ba7
xujianwen <xujianwen@ruijie.com.cn>
2019-12-09 15:47:14 +0800

    测试前后端传值的几种方式

--- app.js
+++ app.js
@@ -22,13 +22,37 @@ app.get('/mocktest',function(req,res){
     res.json(data)
 });
 
-app.get('/test',function(req,res){
+app.get('/test',function(req,res) {
     res.status(200),
     res.json({ str: 'test api string' });
 });
 
-var server = app.listen(3000, function () {
+// 请求测试
+app.get('/test1/:id?', function(req, res) {
+  const params = req.params;
+  const query = req.query;
+  res.status(200),
+  res.json({ data: {
+    params,
+    query
+  }});
+});
+
+app.post('/test2', function(req, res) {
+  const params = req.params;
+  const body = req.body;
+  const query = req.query;
+  console.log('测试 body', body);
+  res.status(200),
+  res.json({ data: {
+    params,
+    query,
+    body
+  }});
+});
+
+var server = app.listen(3010, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
-})
+});
